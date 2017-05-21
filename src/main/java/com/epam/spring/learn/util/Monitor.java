07782ab73c51f340f25024d28d4f58acddfe9757
Andrey_Chadov <androusj@mail.ru>
2017-05-21 21:04:37 +0600

    [IMP]  Implemented AOP Monitor

---
+++ src/main/java/com/epam/spring/learn/util/Monitor.java
@@ -0,0 +1,19 @@
+package com.epam.spring.learn.util;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ */
+@Component
+public class Monitor implements ApplicationListener<ApplicationEvent> {
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        System.out.println(event.getClass().getSimpleName() + " > "
+                + event.getSource().toString());
+    }
+}
+
