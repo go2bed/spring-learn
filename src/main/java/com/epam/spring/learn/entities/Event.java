2bf7a710d731b9e2b7a5c64a952b7a98de5fd1b1
Andrey_Chadov <androusj@mail.ru>
2017-05-21 20:35:53 +0600

    [IMP] Used spring SPELL

--- src/main/java/com/epam/spring/learn/entities/Event.java
+++ src/main/java/com/epam/spring/learn/entities/Event.java
@@ -1,6 +1,8 @@
 package com.epam.spring.learn.entities;
 
 import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 
@@ -34,6 +36,25 @@ public class Event {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public static boolean isDay() {
+        SimpleDateFormat parser = new SimpleDateFormat("HH:mm");
+        Date eight = null;
+        Date five = null;
+        try {
+            eight = parser.parse("8:00");
+            five = parser.parse("17:00");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Date currentDate = new Date();
+        if (currentDate.after(eight) && currentDate.before(five)) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
 
