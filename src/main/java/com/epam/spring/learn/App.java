f4c515180b1ef2a641499dda326cb06dd7567aae
Andrey_Chadov <androusj@mail.ru>
2017-05-21 18:18:21 +0600

    [REF] Refactored new loggers

--- src/main/java/com/epam/spring/learn/App.java
+++ src/main/java/com/epam/spring/learn/App.java
@@ -1,42 +1,47 @@
 package com.epam.spring.learn;
 
-import com.epam.spring.learn.loggers.EventLogger;
-import com.epam.spring.learn.loggers.impl.CacheFileEventLogger;
-import com.epam.spring.learn.loggers.impl.ConsoleEventLogger;
 import com.epam.spring.learn.entities.Client;
 import com.epam.spring.learn.entities.Event;
-import com.epam.spring.learn.loggers.impl.FileEventLogger;
+import com.epam.spring.learn.entities.EventType;
+import com.epam.spring.learn.loggers.EventLogger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Map;
 
 
 public class App {
 
+    Map<EventType, EventLogger> loggers;
 
     Client client;
 
     Event event;
 
-    ConsoleEventLogger consoleEventLoggerImpl;
+    EventLogger eventLogger;
 
-    FileEventLogger fileEventLogger;
-
-    CacheFileEventLogger cacheFileEventLogger;
+    EventLogger defaultEventLogger;
 
 
-    public App(Client client, EventLogger consoleEventLoggerImpl, Event event) {
+    public App(Client client, EventLogger defaultEventLogger,
+               Map<EventType, EventLogger> loggers, Event event) {
         this.client = client;
-        this.fileEventLogger = (CacheFileEventLogger) consoleEventLoggerImpl;
+        this.loggers = loggers;
         this.event = event;
+        this.defaultEventLogger = defaultEventLogger;
     }
 
     public App() {
     }
 
-    public void logEvent(String msg) {
+    public void logEvent(EventType eventType, String msg) {
         String message = msg.replaceAll(client.getId(), client.getFullName());
-        for (int i = 0; i < 7 ; i++) {
-            fileEventLogger.logEvent(event);
+        eventLogger = loggers.get(eventType);
+        if (eventLogger == null) {
+            eventLogger = defaultEventLogger;
+        }
+        for (int i = 0; i < 7; i++) {
+            eventLogger.logEvent(event);
         }
     }
 
@@ -47,8 +52,8 @@ public class App {
         App app = ctx.getBean(App.class, "app");
 
 
-        app.logEvent("Some event for user 1");
-        app.logEvent("Some event for user 2");
+        app.logEvent(EventType.INFO, "Some event for user 1");
+        app.logEvent(EventType.ERROR, "Some event for user 2");
 
     }
 }
